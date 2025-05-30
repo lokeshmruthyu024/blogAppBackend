af29f5ae8f9c01a893221c1ac14c48de15ae5b5b
lokeshmruthyu024 <lokeshnagaraj082@gmail.com>
2025-05-30 16:15:21 +0530

    added cors

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser';
 import { v2 as cloudinary } from 'cloudinary'
 import authRoutes from './routes/auth.routes.js'
 import postRoutes from './routes/post.routes.js';
+import cors from 'cors';
 
 import connectMongoDB from './db/connectMongo.js';
 
@@ -13,6 +14,27 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const allowedOrigins = [
+  'http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'http://localhost:3003', 'http://localhost:3004',               // local dev frontend
+  'https://blogapplokesh.netlify.app/' // replace with your actual Netlify URL
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // allow requests with no origin like Postman or curl
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true, // Allow cookies/auth headers to be sent
+}));
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Blog API');
+})
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
