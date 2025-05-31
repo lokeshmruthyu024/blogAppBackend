7b282ee6a86d43bd0a68fa218273025ccd7f4f93
lokeshmruthyu024 <lokeshnagaraj082@gmail.com>
2025-05-31 13:49:00 +0530

    changes

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,13 +1,12 @@
-import jwt from "jsonwebtoken";
-
 export const generateTokenAndSetCookie = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "15d",
-  })
+  });
   res.cookie('jwt', token, {
     maxAge: 15 * 24 * 60 * 60 * 1000,
-    httpOnly: true, //prevent XSS attacks cross sites scripting the attacks
-    sameSite: "none", //CSRF attacks cross-sites request forgery attacks
-    secure: process.env.NODE_ENV !== "development",
-  })
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",  // allow cross-origin cookie
+    secure: process.env.NODE_ENV !== "development",  // must be true for SameSite=None in production
+  });
 }
